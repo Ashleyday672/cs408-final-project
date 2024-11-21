8f56140c4c434b52b06cef0b12bea0352b1d8036
Ashley Day <ashleyday672@u.boisestate.edu>
2024-11-21 16:38:13 -0700

    tests added

--- test/test.js
+++ test/test.js
@@ -1,11 +1,33 @@
-import {sayHello} from '../js/main.js';
+import { sayHello } from '../js/main.js';
 
-QUnit.module('hello', function() {
+QUnit.module('Basic Functionality', function () {
+  QUnit.test('sayHello function should return "hello"', function (assert) {
+    const result = sayHello();
+    assert.equal(result, 'hello', 'sayHello returned "hello"');
+  });
+});
 
-    QUnit.test('make sure the hello function says hello', function(assert) {
-        var result = sayHello();
-        assert.equal(result, 'hello');
-    });
+QUnit.module('Sanitization', function () {
+  QUnit.test('Sanitizing inputs', function (assert) {
+    const sanitized = "<script>alert('x')</script>".replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    assert.equal(sanitized, "&lt;script&gt;alert('x')&lt;/script&gt;", 'Sanitization works as expected');
+  });
+});
 
+QUnit.module('Project Management', function () {
+  QUnit.test('Adding projects', function (assert) {
+    const projects = [];
+    const newProject = { title: "Test Project", dueDate: "2024-12-01" };
+    projects.push(newProject);
 
+    assert.equal(projects.length, 1, 'Project was added');
+    assert.deepEqual(projects[0], newProject, 'Added project matches the expected object');
+  });
+
+  QUnit.test('Deleting projects', function (assert) {
+    const projects = [{ title: "Test Project", dueDate: "2024-12-01" }];
+    projects.pop();
+
+    assert.equal(projects.length, 0, 'Project was deleted');
+  });
 });
